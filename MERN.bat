f681874a9c08e1ece89fe28430f77abc5ad62f4e
Baraa Alshaer <Baraa.Programmer@gmail.com>
2024-04-24 17:26:38 +0300

    first commit

---
+++ MERN.bat
@@ -0,0 +1,5 @@
+@echo off
+start "" "C:\Users\XPRISTO\AppData\Local\Postman\Postman.exe"
+start "" "C:\Users\XPRISTO\AppData\Local\Programs\Microsoft VS Code\Code.exe"
+start "" "C:\Users\XPRISTO\AppData\Local\MongoDBCompass\MongoDBCompass.exe"
+start "" "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
